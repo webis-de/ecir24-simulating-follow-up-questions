5f6b215f348780d187cf95a5048d74eca3b7a1bb
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2023-09-04 00:13:39 +0200

    adapt treccast data to unified format

--- src/bash/parse-treccast.sh
+++ src/bash/parse-treccast.sh
@@ -19,22 +19,40 @@ for input in $@;do
 
          if (cindex == 1) {
            delete history
-           history[cindex] = "{\"turnNumber\":\""turnNumber"\",\"user\":\""utterance"\"}"
+           history[cindex] = "{\"index\":1,\"user_responses\":[\""utterance"\"]}"
+           informationNeed = utterance
          } else {
            if (!(conversation"-"turnNumber in printed)) {
-             printf "%s", "{\"conversation\":"conversation",\"turnNumber\":\""turnNumber"\",\"system\":\""lastResponse"\",\"user\":\""utterance"\",\"history\":["
+             printf "%s", "{\"id\":\"cast-"conversation"-"turnNumber"\",\"conversation_id\":\"cast-"conversation"\",\"system\":\""lastResponse"\",\"user_responses\":[\""utterance"\"],\"previous_turns\":["
              for (i = 1; i < cindex; i += 1) {
                if (i > 1) { printf "," }
                printf "%s", history[i]
              }
-             printf "]}\n"
+             printf "],\"information_need\":\""informationNeed"\"}\n"
 
              printed[conversation"-"turnNumber] = 1
            }
-           history[cindex] = "{\"turnNumber\":\""turnNumber"\",\"system\":\""lastResponse"\",\"user\":\""utterance"\"}"
+           history[cindex] = "{\"index\":"cindex",\"system\":\""lastResponse"\",\"user_responses\":[\""utterance"\"]}"
          }
 
          lastResponse = response
-       }'
+       }' \
+     | python3 -c '
+import sys, json
+lastSystem = ""
+lastRecord = ""
+for line in sys.stdin:
+  record = json.loads(line)
+  if record["system"] == lastSystem:
+    lastRecord["user_responses"].append(record["user_responses"][0]);
+  else:
+    if lastSystem != "":
+      json.dump(lastRecord, sys.stdout)
+      print()
+    lastSystem = record["system"]
+    lastRecord = record
+json.dump(lastRecord, sys.stdout)
+print()
+         '
 done
 
