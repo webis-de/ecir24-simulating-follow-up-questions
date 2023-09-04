6acfa606f0677d657b0704312b04a9a255cfee21
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2023-09-04 14:50:40 +0200

    reverse conversation for label studio

--- src/bash/conversation-to-label-studio.sh
+++ src/bash/conversation-to-label-studio.sh
@@ -2,9 +2,20 @@
 
 for input in $@;do
   cat $input \
-    | jq '. as $context | .user_responses[] | . as $response | $context | .user_response = $response' \
-    | jq '. as $context | .previous_turns | map(. | "System " + (.index|tostring) + ": " + .system + "\n\nUser " + (.index|tostring) + ": " + .user_responses[0] + "\n\n") | join("") | . as $history | $context | .history = "User 0: " + .information_need + "\n\n" + $history + "System " + (.previous_turns|length+1|tostring) + ": " + .system + "\n\nUser " + (.previous_turns|length+1|tostring) + ": " + .user_response | .turns = (.previous_turns|length+1)' \
-    | jq '{"id":.id,"text":.history,"turns":.turns}' \
+    | jq '. as $context
+        | .user_responses[]
+        | . as $response
+        | $context
+        | .user_response = $response' \
+    | jq '. as $context
+        | .previous_turns
+        | reverse
+        | map(. | "User " + (.index|tostring) + ": " + .user_responses[0] + "\n\nSystem " + (.index|tostring) + ": " + .system + "\n\n")
+        | join("")
+        | . as $history
+        | $context
+        | .history = "System " + (.previous_turns|length+1|tostring) + ": " + .system + "\n\n" + $history + "User 0: " + .information_need + "\n\n"' \
+    | jq '{"id":.id,"history":.history,"user_response":.user_response}' \
     | jq '{"id":.id,"data":.}' \
     | jq -s '.'
 done
